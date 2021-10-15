770b7b9c666d687b4e2e4edb2fe17f548d5aadf2
Jianhong Chen <chen_jianhong608@hotmail.com>
2021-10-14 23:40:01 -0400

    third commit

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ const inputContainer = document.getElementById("input-container");
 const countdownForm = document.getElementById("countdownForm");
 const dateEl = document.getElementById("date-picker");
 
+let countdownTitle = "";
+let countdownDate = "";
+
 // Set date input min with today's date (GMT-0400 (Eastern Daylight Time))
 const tzOffset = (new Date().getTimezoneOffset() * 60000);
 const today = new Date(Date.now() - tzOffset).toISOString().split("T")[0];
 dateEl.setAttribute("min", today);
 
+// Take values from form input
+function updateCountdown(event) {
+    event.preventDefault();
+    countdownTitle = event.srcElement[0].value;
+    countdownDate = event.srcElement[1].value;
+    console.log(countdownTitle, countdownDate);
+}
+
+// Event listeners
+countdownForm.addEventListener("submit", updateCountdown);
